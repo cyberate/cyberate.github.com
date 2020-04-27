538f643d8d8f3772509c9e1d6dfb2e9f9c279408
Ryan <castro.rry@gmail.com>
2020-04-27 15:29:17 -0300

    Initial commit

---
+++ precache-manifest.27a2d9bfb2963bddedd8c1fd5a1d5fdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8791074478cb7169d934174f2c4fc359",
+    "url": "/index.html"
+  },
+  {
+    "revision": "594fcb3c93c33efb8dbc",
+    "url": "/static/js/2.2e9a7533.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2e9a7533.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42185cbf689f5ff5d899",
+    "url": "/static/js/main.f50fd6cf.chunk.js"
+  },
+  {
+    "revision": "a82b52f3e3a941c7334d",
+    "url": "/static/js/runtime-main.302eeb64.js"
+  }
+]);
